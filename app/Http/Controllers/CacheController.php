9c2c150bbce4f87bf739f81163320a69510b72ff
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-03-24 16:46:00 +0500

    Carbon now add days for deleting cached data

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class CacheController extends Controller
 {
-    //
+    public function index()
+    {
+        \Cache::put('cacheKey', 'There should be specific value for Key', now()->addDay());
+        dd(\Cache::get('cacheKey'));
+        return view('index');
+    }
 }
